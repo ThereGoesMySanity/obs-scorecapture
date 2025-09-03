b53a570d556e10b85e4441cca929f0f692a0eac7
Will Kennedy <willmlb@sbcglobal.net>
2025-09-03 02:03:57 -0400

    Updated default layout & switched score to double

--- src/ScoreData.hpp
+++ src/ScoreData.hpp
@@ -2,24 +2,24 @@
 
 #include <optional>
 struct ScoreData {
-	int score;
-	std::optional<int> maxScore;
+	double score;
+	std::optional<double> maxScore;
 
-	ScoreData(int _score) : score(_score) {}
+	ScoreData(double _score) : score(_score) {}
 
 	ScoreData(obs_data_t *data)
 	{
-		score = (int)obs_data_get_int(data, "score");
+		score = obs_data_get_double(data, "score");
 		if (obs_data_has_user_value(data, "maxScore")) {
-			maxScore = (int)obs_data_get_int(data, "maxScore");
+			maxScore = obs_data_get_double(data, "maxScore");
 		}
 	}
 
 	void toObsData(obs_data_t *data)
 	{
-		obs_data_set_int(data, "score", score);
+		obs_data_set_double(data, "score", score);
 		if (maxScore) {
-			obs_data_set_int(data, "maxScore", maxScore.value());
+			obs_data_set_double(data, "maxScore", maxScore.value());
 		}
 	}
 };
